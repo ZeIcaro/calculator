48135ac9f7c61167bd8e0ea038f72b5456f4e477
ZeIcaro <89083008+ZeIcaro@users.noreply.github.com>
2025-03-21 00:01:19 -0300

    Add function that returns the number pressed
    as well as the JS animations that highlight the
    corresponding buttons when mouseover.

--- calculations.js
+++ calculations.js
@@ -33,4 +33,40 @@ function operate (operator, x, y) {
 }
 
 
+// Highlights buttons styles on mouseover
 
+function highlightButton () {
+    let buttonsArray = document.querySelectorAll(".button");
+
+    buttonsArray.forEach(function(elem) {
+
+        elem.addEventListener("mouseover", () => {
+            elem.classList.add("highlight");
+        });
+
+        elem.addEventListener("mouseout", () => {
+            elem.classList.remove("highlight");
+        });
+
+    });
+};
+
+// Runs highlightButton function to make them highlighteable
+highlightButton();
+
+
+// Returns the number or character corresponding to each button when pressed
+
+const onClick = function () {
+    console.log(this.id);
+}
+
+function pressNumber () {
+    let numbersArray = document.querySelectorAll(".gray-button");
+
+    numbersArray.forEach(function(elem) {
+        elem.addEventListener("click", (onClick));
+    });
+}
+
+pressNumber();
